a2e821a28a060f2bd808d56822d4f4e62f9a263b
sauanla <Phucnd.zit@gmail.com>
2019-10-10 22:55:58 +0700

    add coppy right

--- sleep/js/scripts.js
+++ sleep/js/scripts.js
@@ -37,61 +37,16 @@
             title = "Buổi chiều cố mà tập trung cho đỡ buồn ngủ";
         } else if (hours >= 18 && hours < 23) {
             shift = "night";
-            title = "Một ngày vất vả rồi - Ngủ thôi nào";
+            title = "Một ngày vất vả rồi </br> Ngủ thôi nào";
         }
         var url = "https://source.unsplash.com/1600x900/?" + shift;
         $.backstretch(url);
         ///
-        $('.section-title').text(title);
+        $('.section-title').html(title);
     }
 
 
-    $(document).ready(function () {
-        // Countdown timer
-        // Change the launchDay according to your needs.
-        // The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
-        // Thus the launchDay below denotes 7 December, 2017.
-        var launchDay = new Date(2019, 12 - 1, 7);
-        $('#timer').countdown({
-            until: launchDay,
-            format: 'dHMS'
-        });
-
-        // Background image
-        //$.backstretch('images/background-1.jpg');
-
-        // Subscribe form
-        $('#newsletter-form').validate({
-            rules: {
-                newsletter_email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                newsletter_email: {
-                    required: 'Email address is required',
-                    email: 'Email address is not valid'
-                }
-            },
-            errorElement: 'span',
-            errorPlacement: function (error, element) {
-                error.appendTo(element.parent());
-            },
-            submitHandler: function (form) {
-                if ($('#newsletter_extra').val() == '') {
-                    $(form).hide();
-                    $('#newsletter-response')
-                        .html('<span class="bounce1"></span><span class="bounce2"></span><span class="bounce3"></span>')
-                        .show()
-                        .animate({ opacity: 1 });
-                    $.post($(form).attr('action'), $(form).serialize(), function (data) {
-                        $('#newsletter-response').html('<p>Thank you for subscribing!</p>');
-                    });
-                }
-                return false;
-            }
-        });
+    $(document).ready(function () { 
 
         // Modal
         $('#modal-open, #modal-close').on('click', function (e) {
